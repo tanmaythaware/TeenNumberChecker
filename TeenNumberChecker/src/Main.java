1e1a985822d39093541333e6df75d916eec3d429
tanmaythaware <85193272+tanmaythaware@users.noreply.github.com>
2021-06-07 23:23:14 +0530

    Add files via upload

---
+++ TeenNumberChecker/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(TeenNumberChecker.hasTeen(14,22,25));
+        System.out.println(TeenNumberChecker.isTeen(25));
+
+    }
+
+}
